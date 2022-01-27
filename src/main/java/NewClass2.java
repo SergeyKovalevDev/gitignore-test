3d07c1621b9cc5b56049e5131ea8717b3d3d04f1
SergeyKoavalev <dip.deep@mail.ru>
2022-01-27 22:46:16 +0300

    added getters and setters

--- src/main/java/NewClass2.java
+++ src/main/java/NewClass2.java
@@ -7,4 +7,27 @@ public class NewClass2 {
         this.c = c;
     }
 
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
 }
