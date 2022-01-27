7a20d4e9cc6a1dc42a61cde563e280f66eb85889
SergeyKoavalev <dip.deep@mail.ru>
2022-01-27 22:41:56 +0300

    added getter and setter

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -4,4 +4,12 @@ public class NewClass {
     public NewClass(String str) {
         this.str = str;
     }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
